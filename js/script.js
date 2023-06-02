7d0a802bbbefc76d484472f0e75ac1682886b90d
Alibek Ergashev <ergashevalibek47@gmail.com>
2023-06-02 15:25:40 +0500

    add a new js structure

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let firstname = " Alibek"
+let surname = " Ergashev"
+let age = 18
+let republic = "Uzbekistan"
+let city = "Karshi"
+let education = " TATU"
+let hobby = " Car drive"
+console.log("My name is"+ firstname +", "+"My surname is" + surname+", " +"My age is " + age + ", " +"My republic is " + republic+ ", " +"My city is " + city+ ", " +"My education is " + education+ ", " + "My hobby is" + hobby)
